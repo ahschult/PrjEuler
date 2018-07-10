a715bcf18fbac83ae91c388a924def850de7f214
ahschult <ahschult@gmail.com>
2018-07-10 04:02:17 +0000

    add soln 1+2, start 3+5

---
+++ problem3.py
@@ -0,0 +1,10 @@
+#The prime factors of 13195 are 5, 7, 13 and 29.
+#What is the largest prime factor of the number 600851475143 ?
+
+x=600851475143
+y=1
+largestPrime=1
+
+while y<x:
+    
+print(largestPrime)
